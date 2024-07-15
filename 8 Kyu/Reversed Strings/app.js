f6c0343018d6e0295b3297ef4fd1d980da51bd80
randi089 <randifebriadi@gmail.com>
2024-07-15 09:11:24 +0700

    edit

--- 8 Kyu/Reversed Strings/app.js
+++ 8 Kyu/Reversed Strings/app.js
@@ -1,5 +1,6 @@
 function rstring(string) {
-  return string.split("").reverse().join("");
+  //   return string.split("").reverse().join("");
+  return [...string].reverse().join("");
 }
 
 console.log(rstring("Randi"));
