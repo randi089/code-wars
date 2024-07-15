fcf0d8b392299925293201235eb80a28d93c120b
randi089 <randifebriadi@gmail.com>
2024-07-15 10:44:20 +0700

    string repeat

---
+++ 8 Kyu/String repeat/app.js
@@ -0,0 +1,11 @@
+function strr(n, s) {
+  //   let string = "";
+  //   for (let i = 0; i < n; i++) {
+  //     string += s;
+  //   }
+  //   return string;
+
+  return s.repeat(n);
+}
+
+console.log(strr(5, "tes"));
