08f05bc755d56d6ad95b6dfbc9e657891d758bf3
randi089 <randifebriadi@gmail.com>
2024-07-15 20:28:11 +0700

    convert to number

---
+++ 8 Kyu/Convert a String to a Number!/app.js
@@ -0,0 +1,13 @@
+// function snumber(string) {
+// return eval(string);
+
+//   return parseInt(string);
+
+//   return parseFloat(string);
+
+//   return +string;
+// }
+
+const snumber = Number;
+
+console.log(typeof snumber("-100"));
