29c2eab180644db6017fee2dd77a708604f4e3fb
randi089 <randifebriadi@gmail.com>
2024-07-15 08:44:54 +0700

    edit

--- 8 Kyu/Grasshopper - Summation/app.js
+++ 8 Kyu/Grasshopper - Summation/app.js
@@ -1,9 +1,13 @@
 function jumlah(n) {
-  let total = 0;
-  for (let i = 1; i <= n; i++) {
-    total += i;
-  }
-  return total;
+  //   let total = 0;
+  //   for (let i = 1; i <= n; i++) {
+  //     total += i;
+  //   }
+  //   return total;
+
+  //   return n ? n + jumlah(n - 1) : 0;
+
+  return (n * (n + 1)) / 2;
 }
 
 console.log("Hasil : " + jumlah(8));
