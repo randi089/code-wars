25d556c62aacead1f0f9e37353b2194e9f6fbf88
randi089 <randifebriadi@gmail.com>
2024-07-15 20:38:36 +0700

    divisible

---
+++ 8 Kyu/Is n divisible by x and y/app.js
@@ -0,0 +1,12 @@
+function isDivisible(n, x, y) {
+  //   return n % x == 0 && n % y == 0 ? true : false;
+
+  return n % x == 0 && n % y == 0;
+}
+
+console.log(isDivisible(10, 3, 5));
+
+console.log(isDivisible(3, 3, 4), false);
+console.log(isDivisible(12, 3, 4), true);
+console.log(isDivisible(8, 3, 4), false);
+console.log(isDivisible(48, 3, 4), true);
