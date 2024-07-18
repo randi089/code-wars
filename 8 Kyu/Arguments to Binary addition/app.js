c1dbd74275bf882d802ed08b7d8fb5b38331dedf
randi089 <randifebriadi@gmail.com>
2024-07-18 21:15:55 +0700

    toString(2)

---
+++ 8 Kyu/Arguments to Binary addition/app.js
@@ -0,0 +1,9 @@
+function arr2bin(arr) {
+  return arr.reduce((x, y) => x + (typeof y == "number" ? y : 0), 0).toString(2);
+}
+
+console.log(arr2bin([1, 2]), "11");
+console.log(arr2bin([1, 2, 3, 4, 5]), "1111");
+console.log(arr2bin([1, 10, 100, 1000]), "10001010111");
+console.log(arr2bin([null]), "0");
+console.log(arr2bin([true, true, false, 15]), "1111");
