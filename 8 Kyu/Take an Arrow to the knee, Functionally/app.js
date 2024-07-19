0827e94e5981dff03e151aa40516909699fbc277
randi089 <randifebriadi@gmail.com>
2024-07-19 20:15:26 +0700

    take..

---
+++ 8 Kyu/Take an Arrow to the knee, Functionally/app.js
@@ -0,0 +1,5 @@
+var ArrowFunc = function (arr) {
+  return arr.map((m) => String.fromCharCode(m)).join(""); //Complete this function
+};
+console.log(ArrowFunc([84, 101, 115, 116]), "Test", "Convert those numbers to letters");
+console.log(ArrowFunc([70, 85, 83, 32, 82, 79, 72, 32, 68, 65, 72]), "FUS ROH DAH", "Keep it up!");
