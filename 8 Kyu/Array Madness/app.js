c31515854ce851173c347d1c26c6ed878e3a1bcc
Randi Febriadi <randifebriadi@gmail.com>
2024-08-18 07:38:54 +0700

    array madness

---
+++ 8 Kyu/Array Madness/app.js
@@ -0,0 +1,15 @@
+function arrayMadness(a, b) {
+  return a.reduce((acc, value) => acc + value ** 2, 0) > b.reduce((acc, value) => acc + value ** 3, 0);
+}
+
+console.log(arrayMadness([4, 5, 6], [1, 2, 3]), true);
+console.log(arrayMadness([5, 6, 7], [4, 5, 6]), false);
+console.log(arrayMadness([4, 5, 6], [3, 4, 5]), false);
+console.log(arrayMadness([3, 4, 5], [2, 3, 4]), false);
+console.log(arrayMadness([2, 3, 4], [1, 2, 3]), false);
+console.log(arrayMadness([1, 2, 3], [0, 1, 2]), true);
+console.log(arrayMadness([5, 3, 2, 4, 1], [15]), false);
+console.log(arrayMadness([2, 5, 3, 4, 1], [3, 3, 3, 3, 3]), false);
+console.log(arrayMadness([1, 3, 5, 2, 4], [2, 2, 2, 2, 2, 2, 2, 1]), false);
+console.log(arrayMadness([1, 2, 3, 4, 5], [2, 2, 2, 2, 2, 2, 1, 1, 1]), true);
+console.log(arrayMadness([2, 4, 6, 8, 10, 12, 14], [1, 3, 5, 7, 9, 11, 13]), false);
